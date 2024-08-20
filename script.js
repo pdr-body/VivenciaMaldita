ef2b21e45fecdfa69165c120231fdc637428ed80
pdr-body <pedrof8g15@gmail.com>
2024-08-20 12:27:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-avanca.forEach (button => {
-    button.addEventListener('click', function(){
+avanca.forEach(button => {
+    button.addEventListener('click', function() {
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
-    })
-})
+    });
+});
+
+var backgroundMusic = document.getElementById("backgroundMusic");
+var startSound = document.getElementById("startSound");
+
+window.onload = function() {
+    console.log("Window loaded. Attempting to play background music.");
+    backgroundMusic.play().then(() => {
+        console.log("Background music started playing.");
+    }).catch((error) => {
+        console.error("Failed to play background music:", error);
+    });
+};
+
+function startGame() {
+    console.log("Start button clicked.");
+    startSound.play().then(() => {
+        console.log("Start sound played.");
+        backgroundMusic.pause();
+    }).catch((error) => {
+        console.error("Failed to play start sound:", error);
+    });
+}
+
+
+
