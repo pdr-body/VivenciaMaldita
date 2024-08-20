aa298f01137bdc828043db639816a6e36557256d
pdr-body <pedrof8g15@gmail.com>
2024-08-20 15:44:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,23 +13,9 @@ avanca.forEach(button => {
 var backgroundMusic = document.getElementById("backgroundMusic");
 var StartSound = document.getElementById("StartSound");
 
-window.onload = function() {
-    console.log("Window loaded. Attempting to play background music.");
-    backgroundMusic.play().then(() => {
-        console.log("background music started playing.");
-    }).catch((error) => {
-        console.error("Failed to play background music:", error);
-    });
-};
-
 function startGame() {
-    console.log("Start button clicked.");
-    StartSound.play().then(() => {
-        console.log("Start sound played.");
-        backgroundMusic.pause();
-    }).catch((error) => {
-        console.error("Failed to play start sound:", error);
-    });
+    StartSound.play();
+    backgroundMusic.play();
 }
 
 
